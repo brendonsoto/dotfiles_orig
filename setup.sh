1eb2c50e61643b6a7d64e876b17208aeb801e289
brendon <brendon.soto@gmail.com>
2020-03-29 14:52:55 -0400

    Fixed problems with .config symlinks and updates to coc

--- setup.sh
+++ setup.sh
@@ -5,19 +5,22 @@
 dotfileDir=$(pwd)
 
 # The ".config" dir might already exist so check before symlinking
-# if [ -d "$dotfileDir/.config" ]; then
-# 	ln -s "$dotfileDir/.config/*" "$HOME/.config/"
-# else
-# 	ln -s "$dotfileDir/.config" "$HOME/.config"
-# fi
+if [ -d "$dotfileDir/.config" ]; then
+	echo "Looping"
+	for dir in .config/*; do
+		ln -s "$dotfileDir/$dir" "$HOME/$dir"
+	done
+else
+	ln -s "$dotfileDir/.config" "$HOME/.config"
+fi
 
 # Symlink other config files
-# ln -s "$dotfileDir/.ctags" "$HOME/.ctags"
-# ln -s "$dotfileDir/.ghci" "$HOME/.ghci"
-# ln -s "$dotfileDir/.gitconfig" "$HOME/.gitconfig"
-# ln -s "$dotfileDir/.ignore" "$HOME/.ignore"
-# ln -s "$dotfileDir/.tmux.conf" "$HOME/.tmux.conf"
-# ln -s "$dotfileDir/.vim" "$HOME/.vim"
+ln -s "$dotfileDir/.ctags" "$HOME/.ctags"
+ln -s "$dotfileDir/.ghci" "$HOME/.ghci"
+ln -s "$dotfileDir/.gitconfig" "$HOME/.gitconfig"
+ln -s "$dotfileDir/.ignore" "$HOME/.ignore"
+ln -s "$dotfileDir/.tmux.conf" "$HOME/.tmux.conf"
+ln -s "$dotfileDir/.vim" "$HOME/.vim"
 ln -s "$dotfileDir/.vim/vimrc" "$HOME/.vimrc"
 ln -s "$dotfileDir/.Xmodmap" "$HOME/.Xmodmap"
 ln -s "$dotfileDir/.zshrc" "$HOME/.zshrc"
