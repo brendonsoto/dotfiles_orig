c4a9976f6403c9f6ad201301d6decbc51791f457
brendon <brendon.soto@gmail.com>
2021-04-02 17:56:56 -0400

    updated setup and removed old config files

--- setup.sh
+++ setup.sh
@@ -1,41 +1,70 @@
 #! /bin/sh
 # A script to move all of my config files when setting up a new machine
 
-# Shortcut to the dir where this file is executed
 dotfileDir="$HOME/dotfiles"
-dotfiles=(
+files=(
   ".ctags"
   ".ghci"
   ".gitignore"
   ".tmux.conf"
   ".vim"
-  ".vim/vimrc"
+  "vimrc"
   ".Xmodmap"
   ".zshrc"
 )
 
+
 # Link dot files
-for dotfile in "${dotfiles[@]}";do
- ln -sf "$dotfileDir/$dotfile" "$HOME/$dotfile"
+echo "Creating symlinks..."
+
+for file in "${files[@]}";do
+  if [ $file = "vimrc" ]; then
+    echo "VIMRC"
+    ln -sFfv "$dotfileDir/.vim/$file" "$HOME/.$file"
+  else
+    echo "NOT VIMRC"
+    ln -sFfnv "$dotfileDir/$file" "$HOME/$file"
+  fi
 done
 
 # The ".config" dir might already exist so check before symlinking
-# The -d is to say "does $dotfile/.config exist AND is a directory"
+# The -d is to say "does $dotfileDir/.config exist AND is a directory"
 if [ -d "$dotfileDir/.config" ]; then
 	echo "Looping"
 	for dir in .config/*; do
-		ln -sf "$dotfileDir/.config/$dir" "$HOME/.config/$dir"
+		# No need to include .config after home, it's in dir already!
+		ln -sfFnv "$dotfileDir/$dir" "$HOME/$dir"
 	done
 else
-	ln -sf "$dotfileDir/.config" "$HOME/.config"
+	ln -sfFnv "$dotfileDir/.config" "$HOME/.config"
 fi
 
-# Happy little messages
 echo "Symlinks have been created!"
-echo "Now installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+
+# Check for ZSH
+if [ $ZSH_VERSION ]; then
+  echo "Now installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+
+# Reminder to install useful tools
 echo "Please now download the following:
+> ag
+> asciiquarium
+> fd
+> jq
+> most
 > neovim
+> nvm
+> ranger
 > st/alacritty
-> nvm"
+> tmux
+
+Mac + Homebrew:
+brew install the_silver_searcher asciiquarium fd jq most neovim nvm ranger alacritty tmux
+
+Manjaro:
+sudo pacman the_silver_searcher asciiquarium fd jq most neovim nvm ranger alacritty tmux
+"
