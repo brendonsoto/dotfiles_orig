937b0b850da165b57bd81a170b95f54b2c6e8c68
brendon <brendon.soto@gmail.com>
2021-10-17 19:46:14 -0400

    updating nnn config

--- .config/nvim/lua/setup/nnn.lua
+++ .config/nvim/lua/setup/nnn.lua
@@ -1,14 +1,22 @@
+local env = vim.env
 local map = vim.api.nvim_set_keymap
 local nnn = require("nnn")
 
+env["NNN_PLUG"] = "t:preview-tui"
+
 nnn.setup({
+  picker = {
+    cmd = "tmux new-session nnn -Pp",
+    style = { border = "rounded" },
+    session = "shared",
+  },
   mappings = {
     { "<C-s>", "split" },
     { "<C-t>", "tsplit" },
     { "<C-v>", "vsplit" },
   },
   replace_netrw = "picker",
-  windownav = "<C-l>"
+  window_nav = "<C-l>"
 })
 
 local nnnOptions = " -H<cr>"
