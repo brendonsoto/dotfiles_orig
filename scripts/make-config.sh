9bb115f0fc829d4a7145fd85503dc424aaf11242
brendon <brendon.soto@gmail.com>
2021-10-29 21:47:55 -0400

    updated make-config script to include tmux colors

--- scripts/make-config.sh
+++ scripts/make-config.sh
@@ -3,39 +3,52 @@
 current_hour=$(date +%H)
 starting_day_hour=9
 end_day_hour=18 # 12 + 6 for 24 hour clock
-alacritty_dir="$0:a:h/../alacritty"
-files_to_concat=("$alacritty_dir/base.yml")
+dotfiles_dir="$0:a:h/.."
+alacritty_dir="$dotfiles_dir/alacritty"
+alacritty_config_files=("$alacritty_dir/base.yml")
+tmux_dir="$dotfiles_dir/tmux"
+tmux_config_files=("$tmux_dir/base.conf")
 
-echo "Configuring alacritty..."
+echo "Configuring alacritty and tmux..."
 
 # Add colorscheme for time of day
 # NOTE: How can I make this a higher order function?
 if ([ $current_hour -ge $starting_day_hour ]) && ([ $current_hour -le $end_day_hour ]); then
-  echo "...adding alacritty light colorscheme"
-  files_to_concat+=("$alacritty_dir/gruvbox-light.yml")
+  echo "...adding light colorscheme"
+  alacritty_config_files+=("$alacritty_dir/gruvbox-light.yml")
+  tmux_config_files+=("$tmux_dir/gruvbox-light.conf")
 else
-  echo "...adding alacritty dark colorscheme"
-  files_to_concat+=("$alacritty_dir/gruvbox-dark.yml")
+  echo "...adding dark colorscheme"
+  alacritty_config_files+=("$alacritty_dir/gruvbox-dark.yml")
+  tmux_config_files+=("$tmux_dir/gruvbox-dark.conf")
 fi
 
 # Add any OS-specific configs + keybindings
 if [[ `uname` == "Darwin" ]]; then
   echo "...adding macOS alacritty config"
-  files_to_concat+=("$alacritty_dir/macos.yml")
+  alacritty_config_files+=("$alacritty_dir/macos.yml")
 else
   echo "...adding linux alacritty config"
-  files_to_concat+=("$alacritty_dir/linux.yml")
+  alacritty_config_files+=("$alacritty_dir/linux.yml")
 fi
 
-# Delete alacritty file if exists
-if [[ -f "~/.alacritty.yml" ]]; then
+# Delete config files if exists
+if [[ -a ~/.alacritty.yml ]]; then
   echo "...deleting existing ~/.alacritty.yml"
   rm ~/.alacritty.yml
 fi
+if [[ -a ~/.tmux.conf ]]; then
+  echo "...deleting existing ~/.tmux.conf"
+  rm ~/.tmux.conf
+fi
 
 echo "...writing alacritty file"
-for f in $files_to_concat; do (cat "${f}"; echo) >> ~/.alacritty.yml; done
+for f in $alacritty_config_files; do (cat "${f}"; echo) >> ~/.alacritty.yml; done
 echo "Finished creating ~/.alacritty.yml !"
+
+echo "...writing tmux file"
+for f in $tmux_config_files; do (cat "${f}"; echo) >> ~/.tmux.conf; done
+echo "Finished creating ~/.tmux.conf !"
 
 # Toggle macOS dark mode
 if [[ `uname` == "Darwin" ]]; then
