cd3f91ae0493da84ddf07115afb36a1f63a811fa
brendon <brendon.soto@gmail.com>
2021-11-27 16:45:22 -0500

    Commenting out some autocmds from neoformat
    Has been slowing down writing some files

--- .config/nvim/lua/plugins/neoformat.lua
+++ .config/nvim/lua/plugins/neoformat.lua
@@ -17,9 +17,9 @@ g.neoformat_only_msg_on_error = 1
 g.neoformat_try_node_exe = 1
 
 -- Format on save
-vim.cmd([[
-  augroup fmt
-    autocmd!
-    au BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
-  augroup END
-]])
+-- vim.cmd([[
+--   augroup fmt
+--     autocmd!
+--     au BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
+--   augroup END
+-- ]])
